34592ff835ed28125301c0f5f7c457e6be2443a1
Snekyy <snekyy@protonmail.com>
2020-12-17 11:00:25 +0300

    new segments

--- .config/powerline-shell/themes/green.py
+++ .config/powerline-shell/themes/green.py
@@ -1,26 +1,49 @@
 from powerline_shell.themes.default import DefaultColor
 
+COLOR = {
+    "black": 0,
+    "white": 15,
+    "grey": 236,
+    "orange": 214,
+    "green": 148,
+    "blue": 75,
+    "purple": 56,
+    "red": 125,
+    "pink": 132
+}
 
 class Color(DefaultColor):
-   
+
     HOME_SPECIAL_DISPLAY = True
-    HOME_FG = 15
-    HOME_BG = 236 
-    
-    PATH_FG = 0 
-    PATH_BG = 214 
-    SEPARATOR_FG = 0
-    
-    CWD_FG = 0
+    HOME_FG = COLOR["white"]
+    HOME_BG = COLOR["grey"]
+
+    PATH_FG = COLOR["black"]
+    PATH_BG = COLOR["orange"]
+    SEPARATOR_FG = COLOR["black"]
+
+    CWD_FG = COLOR["black"]
 
     # Second line
-    TIME_FG = 0
-    TIME_BG = 214 
+    TIME_FG = COLOR["black"]
+    TIME_BG = COLOR["orange"]
 
-    HOSTNAME_FG = 0 
-    HOSTNAME_BG = 148 # 154 - green
+    SSH_FG = COLOR["black"]
+    SSH_BG = COLOR["pink"] 
 
-    CMD_PASSED_FG = 15
-    CMD_PASSED_BG = 236 # dark grey
-    CMD_FAILED_FG = 15
-    CMD_FAILED_BG = 125 # deep pink
+    VIRTUAL_ENV_FG = COLOR["black"]
+    VIRTUAL_ENV_BG = COLOR["green"]
+
+    REPO_CLEAN_FG = COLOR["black"]
+    REPO_CLEAN_BG = COLOR["blue"]
+    REPO_DIRTY_FG = COLOR["black"] 
+    REPO_DIRTY_BG = COLOR["blue"] 
+    GIT_NOTSTAGED_FG = COLOR["white"] 
+    GIT_NOTSTAGED_BG = COLOR["purple"]
+    GIT_UNTRACKED_FG = COLOR["white"] 
+    GIT_UNTRACKED_BG = COLOR["purple"]
+
+    CMD_PASSED_FG = COLOR["white"] 
+    CMD_PASSED_BG = COLOR["grey"] 
+    CMD_FAILED_FG = COLOR["white"] 
+    CMD_FAILED_BG = COLOR["red"]
