77e152fdc7dad4214015066244eada99b25cbca7
Snekyy <snekyy@protonmail.com>
2021-06-29 22:51:53 +0300

    polybar update
    New color scheme, new modules and other improvements

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-# Clear previous logs
- > ~/.config/polybar/error.log
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Clear previous logs
+ > ~/.config/polybar/polybar.log
+
 # Launch polybar 
-polybar main -c ~/.config/polybar/config.ini --log-level="warning" 2>>$HOME/.config/polybar/error.log 1>&2 &
+polybar main -c ~/.config/polybar/config.ini --log-level="warning" 2>>$HOME/.config/polybar/polybar.log 1>&2 &
