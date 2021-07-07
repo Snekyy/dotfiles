38102f2670e7483987898e44ce6867ffb013acc8
Snekyy <snekyy@protonmail.com>
2021-07-07 14:42:34 +0300

    Picom configuration update

--- cfg/config/picom/launch.sh
+++ cfg/config/picom/launch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-# Clear previous logs
- > /home/snekyy/.config/picom/error.log
-
 # Terminate already running picom
 killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+while pgrep -u $UID -x picom >/dev/null; do sleep 0.5; done
+
+# Clear previous logs
+ > /home/snekyy/.config/picom/error.log
 
 # Launch picom
 picom --experimental-backends --config $HOME/.config/picom/picom.conf --log-level "WARN" --show-all-xerrors 2>>$HOME/.config/picom/error.log &
