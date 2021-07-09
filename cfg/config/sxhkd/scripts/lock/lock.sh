dd885e1fa7d4b3d29e5e842f97fdb8c266c8dbc3
Snekyy <snekyy@protonmail.com>
2021-07-09 15:52:04 +0300

    i3lock config update
    New theme and other config changes for i3lock config due to programm update.

---
+++ cfg/config/sxhkd/scripts/lock/lock.sh
@@ -0,0 +1,55 @@
+#aur/i3lock-color-git
+
+# Switch foreign layout
+setxkbmap us
+
+insidevercolor=0f0f0f00
+insidewrongcolor=0f0f0f00
+insidecolor=aaaaaa22
+
+ringvercolor=0f0f0faa
+ringwrongcolor=d84040aa
+ringcolor=888888aa
+
+linecolor=303030ff
+
+keyhlcolor=ffaf00aa
+bshlcolor=d84040
+
+separatorcolor=ffffff00
+
+i3lock \
+	--ignore-empty-password \
+	--indicator \
+	--image=/home/snekyy/.config/sxhkd/scripts/lock/lock.png \
+	--centered \
+	--blur=3 \
+	\
+	--pass-media-keys \
+	--pass-screen-keys \
+	--pass-power-keys \
+	--pass-volume-keys \
+	\
+	--inside-color=$insidecolor \
+	--insidever-color=$insidevercolor \
+	--insidewrong-color=$insidewrongcolor \
+	\
+	--radius=80 \
+	--ring-width=10 \
+	--ring-color=$ringcolor \
+	--ringver-color=$ringvercolor \
+	--ringwrong-color=$ringwrongcolor \
+	\
+	--line-color=$linecolor \
+	\
+	--keyhl-color=$keyhlcolor \
+	--bshl-color=$bshlcolor \
+	\
+	--separator-color=$separatorcolor \
+	\
+	--verif-text="" \
+	--wrong-text="" \
+	--noinput-tex="" \
+	--lock-text="" \
+	--greeter-text="" \
+	--no-modkey-text \
