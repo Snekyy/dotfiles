e42aa2aa0632a945e9afd1419ea97000e1b2fea5
Snekyy <snekyy@protonmail.com>
2021-07-10 22:42:57 +0300

    Hotfix
    Script was not working correctly

--- setup.sh
+++ setup.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 #
-# Dotfiles installation bash script for Arch based dirstos.
+# Dotfiles installation bash script for Arch based distros.
 # Downloads and installs deps for dotfiles and
-# copies dotfiles to user's home directory.
+# copies dotfiles themselves to user's home directory($HOME).
 #
-# ! Requires yay to install deps !
+# Original dotfiles placed at $HOME/dotfiles.
+# Dotfiles that already exist(users old dotfiles)
+# and should be replaced with new will be
+# moved to $HOME/old_dotfiles.
+# Dotfiles that used by real programms are just symlinks of
+# files from $HOME/dotfiles. This way is really useful for
+# managing dotfiles in system.
+#
+# ! Script should be executed from repo dir to work correctly !!
 #
 
-# Colors
+
+# Colors for messages
 red="\033[01;31m"
 green="\033[01;32m"
 reset="\033[00m"
+
 
 #
 # SYSTEM CHECK
@@ -22,15 +32,14 @@ if ! grep "$distros" /etc/*-release > /dev/null 2>&1; then
 	echo -e "-> $red[Error]$reset: Your OS is not Arch based.\nAborting.." && exit 1
 fi
 
-# TODO: git availability check, yay "no" choice completion
 # Check yay availability
 if ! which yay > /dev/null 2>&1; then
 	echo -e "-> $red[Error]$reset: yay is required to install dependencies!"
 	read -r -p "-> Would you like to install yay? [Y/n]: " user_choice
 	case $user_choice in
 		[yY]|""|"Yes"|"yes")
-			git clone https://aur.archlinux.org/yay.git $HOME/.script_setup
-			cd $HOME/.script_setup && makepkg -sri && cd .. && rm -rf .script_setup
+			git clone https://aur.archlinux.org/yay.git $HOME/.script_setup/yay
+			cd $HOME/.script_setup/yay && makepkg -sri; cd .. && rm -rf yay
 			echo -e "$green[*]$reset Yay is installed"
 		;;
 		[nN])
@@ -44,15 +53,17 @@ else
 	echo -e "$green[*]$reset Yay is detected"
 fi
 
+
 #
 # VARIABLES
 #
 
 # Repository dir
-repo_dir=$(echo $0 | sed "s/\/setup\.sh$//")
+repo_dir=$PWD
 # New/old dotfiles folder
 dots="$HOME/dotfiles"
 old_dots="$HOME/old_dotfiles"
+
 
 #
 # DEPENDENCIES INSTALLATION
@@ -61,35 +72,83 @@ old_dots="$HOME/old_dotfiles"
 # Delete all comments, empty lines
 # add spaces between packages.
 dependencies=$(sed "/^#\|^$/d" $repo_dir/dependencies.txt | tr "\n" " ")
+
 # Update system
 echo "-> Updating operation system.."
-sudo pacman -Suy --noconfirm --disable-download-timeout
-echo -e "$green[*]$reset System updated"
+sudo pacman -Suy --disable-download-timeout --needed
+# if user acually updated system
+[ $? -eq 0 ] && echo -e "$green[*]$reset System updated"
+
 # Install deps
 echo "-> Downloading/installing dependencies.."
-yay -S $dependencies --answerclean All --answerdiff None --noconfirm --disable-download-timeout
-echo -e "$green[*]$reset Dependencies are installed"
+yay -S $dependencies --answerclean All --answerdiff None --disable-download-timeout --needed
+# if user acually installed deps
+[ $? -eq 0 ] && echo -e "$green[*]$reset Dependencies are installed"
+
 
 #
 # INSTALLING DOTFILES
 #
 
 echo "-> Installing dotfiles.."
-mkdir $dots
-mkdir $old_dots
-[ -f "$HOME/.config" ] || mkdir $HOME/.config
-echo "--> Copying new dotfiles to $dots.."
-cp -r $repo_dir/cfg/* $dots
+
+# Prepare home directory.
+
+#TODO: check if the another "dotfiles" is a repo
+#	if it is then give it appropriate name
+# If "dotfiles" directory already exists
+# (it is may be repo) then copy dots from repo
+# to tmp dir, rename "dotfiles" directory,
+# and move tmp files to destination folder.
+if [ -d "$HOME/dotfiles" ]; then
+	mkdir -p $HOME/.script_setup/dotfiles_tmp
+	cp -r $repo_dir/cfg/* $HOME/.script_setup/dotfiles_tmp
+	mv "$HOME/dotfiles" "$HOME/dotfiles_unknown"
+	echo "--> ! Another 'dotfiles' in your home dir renamed to 'dotfiles_unknown' ,"
+	echo "      It is may be a dotfiles repository directory !"
+	mkdir $dots
+	mv $HOME/.script_setup/dotfiles_tmp/* $dots
+	rmdir $HOME/.script_setup/dotfiles_tmp
+# just copy dots
+else
+	mkdir $dots
+	cp $repo_dir/cfg/* -r $dots
+fi
+
+mkdir -p "$old_dots/.config"
+[ -d "$HOME/.config" ] || mkdir "$HOME/.config"
+
+# Get list of dotfiles(without dots - ".")
 cd $dots
-# Prepare files list(without dots)
-files=$(/bin/ls --color=never -1; /bin/ls config --color=never | sed "s/^/config\//" | sed "/config$/d")
+files=$(/bin/ls --color=never -1 | sed "/^config$/d"; /bin/ls config --color=never | sed "s/^/config\//")
+
+# Create symlinks
 for file in $files
 do
-	echo ".$file.."
-	if [ -f "$HOME/.$file" ]; then
-		mv "$HOME/.$file" $old_dots
+	echo ".$file"
+	# If old dotfile has the same name as new
+	# move it to old_dotfiles directory
+	if [ -e "$HOME/.$file" ]; then
+		# If it is a dotfiles from ".config" folder
+		if [ $(echo $file | grep "config" > /dev/null 2>&1; echo $?) -eq 0 ]; then
+			mv "$HOME/.$file" "$old_dots/.config/"
+		else
+			mv "$HOME/.$file" $old_dots
+		fi
 	fi
-	ln -s $dots/$file $HOME/.$file
+	# Create a symlink of orig dotfiles
+	ln -s $dots/$file "$HOME/.$file"
 done
+
 echo "--> Old dotfiles stored into $old_dots"
-echo -e "$green[x]$reset Dotfiles installed successfully"
+
+
+#
+# COMPLETE
+#
+
+# Cleanup tmp files
+rmdir $HOME/.script_setup
+
+# Send message about completion
+echo -e "$green[x]$reset Dotfiles installed successfully!!!"
